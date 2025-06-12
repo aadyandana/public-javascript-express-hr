08976fb9016577dff0d12b2a770e6f6ea444d1e0
agus.adyandana <agus.adyandana@bigio.id>
2025-06-12 21:15:13 +0700

    Feat (attendances): Init feature

---
+++ attendance-service/config/config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+const dotenv = require('dotenv');
+
+dotenv.config({ path: ".env" });
+
+module.exports = {
+  development: {
+    dialect: process.env.DB_DIALECT || "postgres",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USERNAME || "developer",
+    password: process.env.DB_PASSWORD || "12345abcde",
+    database: process.env.DB_NAME || "attendance_hr_app_1",
+  }
+}
